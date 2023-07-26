e81c08eb0694bb94daacfc6bc0f13bb1c94b3ee1
bananaplant7 <brooklyncheng2002@gmail.com>
2023-07-26 15:25:53 -0700

    add functionality to search and button, place data onto newly created html elements

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const img = document.querySelector('img');
+const searchBar = document.getElementById('search')
+const tempInC = document.getElementById('tempInC')
+const tempInF = document.getElementById('tempInF')
+const currentCondition = document.getElementById('currentCondition')
+const btn = document.querySelector('button')
 
-let userLocation = 'Vancouver'
+let userLocation = ''
 
 // TODO:
 // - try catch block, if invalid location throw an alert
 
 
+
+function getSearch() {
+    userLocation = searchBar.value
+}
+
 async function getWeather() {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=3e4ca77f7e714797b21183350231907&q=${userLocation}`,  { mode: 'cors' })
     const responseJSON = await (response.json())
     console.log(responseJSON)
-    console.log(responseJSON.current.temp_c)
-    console.log(responseJSON.current.condition.text)
+
+
+    tempInC.textContent = responseJSON.current.temp_c + " °C"
+
+
+    tempInF.textContent = responseJSON.current.temp_f + " °F"
+
+    currentCondition.textContent = responseJSON.current.condition.text
 
     img.src = "http:" + responseJSON.current.condition.icon
 }
 
+btn.addEventListener("click", () => {getSearch(), getWeather()})
+
+
+getSearch()
 getWeather()
