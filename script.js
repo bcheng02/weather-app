f03a4861592871295be82a23c053a35712fcbe28
bananaplant7 <brooklyncheng2002@gmail.com>
2023-07-27 22:02:16 -0700

    add try catch block to API call, fetch forecast instead of current (same as current with future weather)

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ const btn = document.querySelector('button')
 
 let userLocation = ''
 
-// TODO:
-// - try catch block, if invalid location throw an alert
-
-
 
 function getSearch() {
     userLocation = searchBar.value
 }
 
 async function getWeather() {
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=3e4ca77f7e714797b21183350231907&q=${userLocation}`,  { mode: 'cors' })
-    const responseJSON = await (response.json())
-    console.log(responseJSON)
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=3e4ca77f7e714797b21183350231907&q=${userLocation}`,  { mode: 'cors' })
+        const responseJSON = await (response.json())
+        console.log(responseJSON)
+    
+        tempInC.textContent = responseJSON.current.temp_c + " °C"
+    
+    
+        tempInF.textContent = responseJSON.current.temp_f + " °F"
+    
+        currentCondition.textContent = responseJSON.current.condition.text
+    
+        img.src = "http:" + responseJSON.current.condition.icon
+    } catch (error){
+        alert("invalid location, please enter a city (or the API is down 😔)")
+    }
 
-
-    tempInC.textContent = responseJSON.current.temp_c + " °C"
-
-
-    tempInF.textContent = responseJSON.current.temp_f + " °F"
-
-    currentCondition.textContent = responseJSON.current.condition.text
-
-    img.src = "http:" + responseJSON.current.condition.icon
 }
 
 btn.addEventListener("click", () => {getSearch(), getWeather()})
