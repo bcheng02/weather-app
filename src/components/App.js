07307443002abbc2c4e7b1f1b8b5b173dd54970c
bananaplant7 <brooklyncheng2002@gmail.com>
2023-08-04 18:55:18 -0700

    clean up

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,8 @@ import WeatherCard from './WeatherCard';
 
 function App() {
 
-  function yo(x) {
-    console.log("yo" + x)
-  }
-
   return (
     <div className="App">
-      {/* <Top onClickSearch={yo} /> */}
 
       <WeatherCard />
 
