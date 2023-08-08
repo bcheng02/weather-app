4b165e2303a5f79b8ade4b77686709b9206b6abd
bananaplant7 <brooklyncheng2002@gmail.com>
2023-08-07 20:39:17 -0700

    render page when click on diff hours on graph, improve style

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,14 +1,63 @@
+import React, { useRef } from 'react';
 import { Chart as ChartJS } from 'chart.js/auto'
-import { Line, Chart } from 'react-chartjs-2';
+import { Line, Chart, getElementAtEvent } from 'react-chartjs-2';
 import { format, getHours, parseISO } from 'date-fns';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-const ChartComponent = ({ weatherData, selectedTab, selectedCard }) => {
+const ChartComponent = ({ weatherData, selectedTab, selectedCard, arrOfSetStates }) => {
+    // Create a ref to the chart
+    const chartRef = useRef();
+
     // only do ALL of the logic if you have the weatherData obj
     if (weatherData[0]) {
 
+        let arrOf48hr = weatherData[0].forecast.forecastday[selectedCard].hour.concat(weatherData[0].forecast.forecastday[selectedCard + 1].hour)
+        // 24 hours starting from current hour thru to the next day (that's why need 48hrs ^)
+        let currentHour = getHours(parseISO(weatherData[0].current.last_updated))
+        let start = currentHour
+        let end = currentHour + 24
+        let arrOf24hr = arrOf48hr.slice(start, end)
+        let arrOf24hrTime = arrOf24hr.map((hour) => format((parseISO(hour.time)), "h aaaa"))
+
+        const onClick = (event) => {
+            let element = getElementAtEvent(chartRef.current, event)
+
+            // it's a vaid data point if getElementAtEvent returns a non-empty array
+            let isDataPoint = element.length !== 0
+            if (isDataPoint) {
+                let selectedHourObj = arrOf24hr[element[0].index]
+
+                arrOfSetStates[0](selectedHourObj.condition.icon)
+                arrOfSetStates[1](Math.round(selectedHourObj.temp_c))
+                arrOfSetStates[2](selectedHourObj.change_of_rain)
+                arrOfSetStates[3](selectedHourObj.humidity)
+                arrOfSetStates[4](Math.round(selectedHourObj.wind_kph))
+                let selectedDate = parseISO(selectedHourObj.time)
+                arrOfSetStates[5](format(selectedDate, "EEEE h:00 aaaa"))
+                arrOfSetStates[6](selectedHourObj.condition.text)
+                // arrOfSetStates[7]()
+                // arrOfSetStates[8]()
+
+
+                // [setCurrentImgSrc,
+                //     setcurrentTemp,
+                //     setPrecip,
+                //     setHumidity,
+                //     setWind,
+                //     setCurrDate,
+                //     setCurrCond,
+                //     setSelectedCard,
+                //     setSelectedHour]
+            }
+
+
+        }
+
+
+
+
         let data = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            labels: arrOf24hrTime,
             datasets: [
                 {
                     label: "placeholder label",
@@ -21,18 +70,54 @@ const ChartComponent = ({ weatherData, selectedTab, selectedCard }) => {
             ],
         }
         let options = {
+            events: [],
+            hover: { mode: null },
+            elements: {
+                point: {
+                    pointStyle: 'rect',
+                    borderWidth: 0,
+                    pointBackgroundColor: 'rgba(0,0,0,0)',
+                    radius: (context) => {
+                        return (context.dataIndex % 3 === 0) ? 40 : 0
+                    },
+                    hoverRadius: (context) => {
+                        return (context.dataIndex % 3 === 0) ? 40 : 0
+                    },
+                    hoverBackgroundColor: 'rgba(0,0,0,0)'
+                },
+            },
             plugins: {
                 legend: {
                     display: false, // Hide legend
                 },
+                datalabels: {
+                    // I've spent over 2 hours trying to push the labels to the top right and I CAN'T FIGURE IT OUT. I'VE TRIED EVERYTHING
+                    // THIS LIBRARY IS SO AWFUL I WANT TO RUN INTO TRAFFIC
+                    align: 'right',
+                    anchor: 'center',
+                    offset: 10,
+                    // PADDING DOESN'T ACTUALLY DO ANYTHING BTW, IT JUST DOES EXACTLY WHAT OFFSET DOES
+                    padding: {
+                        top: 0,
+                        right: 0,
+                        bottom: 0,
+                        left: 0
+                    },
+                    formatter: (value, context) => {
+                        // Round the data value to 2 decimal places after it is displayed
+                        return (context.dataIndex % 3 === 0) ? Math.round(value) : ''
+                    },
+                },
             },
             scales: {
                 y: {
+                    display: false,
                     ticks: {
                         display: false
                     },
                     grid: {
-                        display: false
+                        display: false,
+                        drawBorder: false
                     }
 
                 },
@@ -40,7 +125,7 @@ const ChartComponent = ({ weatherData, selectedTab, selectedCard }) => {
                     ticks: {
                         callback: function (val, index) {
                             // show all labels starting from 2nd and then + 3
-                            if (index == 1 || ((index - 1) % 3 == 0)) {
+                            if ((index - 1) % 3 === 0) {
                                 return this.getLabelForValue(val)
                             } else return ''
                         },
@@ -49,40 +134,34 @@ const ChartComponent = ({ weatherData, selectedTab, selectedCard }) => {
                         minRotation: 0
                     },
                     grid: {
-                        display: false
+                        display: false,
+                        drawBorder: false,
+                    },
+                    border: {
+                        // HAHAHA IT ACTUALLY WORKED I FINALLY FOUND IT 
+                        // AFTER AN HOUR OF SEARCHING THIS LIBRARY SUCKS
+                        color: 'white'
                     }
-
                 }
             },
             maintainAspectRatio: false,
         };
 
-        if (selectedTab == 0) {
-
-            let arrOf48hr = weatherData[0].forecast.forecastday[selectedCard].hour.concat(weatherData[0].forecast.forecastday[selectedCard + 1].hour)
-
-            // 24 hours starting from current hour thru to the next day (that's why need 48hrs ^)
-            let currentHour = getHours(parseISO(weatherData[0].current.last_updated))
-            let start = currentHour
-            let end = currentHour + 24
-            let arrOf24hr = arrOf48hr.slice(start, end)
-
+        if (selectedTab === 0) {
+            // set tab specific data
             let arrOf24hrTemp = arrOf24hr.map((hour) => hour.temp_c)
-            let arrOf24hrTime = arrOf24hr.map((hour) => format((parseISO(hour.time)), "h aaaa"))
-
-            data.labels = arrOf24hrTime
             data.datasets[0].data = arrOf24hrTemp
 
-            let yMax = Math.max(...data.datasets[0].data)
-            let yMin = Math.min(...data.datasets[0].data)
+            // config
+            let yMax = Math.max(...arrOf24hrTemp)
+            let yMin = Math.min(...arrOf24hrTemp)
             let yMargin = yMax * 0.05
-
             options.scales.y.max = Math.round(yMax + yMargin)
             options.scales.y.min = Math.round(yMin - yMargin)
             options.scales.y.ticks.stepSize = 100
             options.scales.y.ticks.beginAtZero = false
 
-        } else if (selectedTab == 1) {
+        } else if (selectedTab === 1) {
             data = {
                 labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
                 datasets: [
@@ -114,7 +193,13 @@ const ChartComponent = ({ weatherData, selectedTab, selectedCard }) => {
 
 
 
-        return <Line data={data} plugins={[ChartDataLabels]} options={options} />;
+        return <Line
+            data={data}
+            plugins={[ChartDataLabels]}
+            options={options}
+            ref={chartRef} // Add the ref here
+            onClick={onClick}
+        />;
     }
 };
 
