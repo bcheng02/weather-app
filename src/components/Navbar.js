2df451b72f1447690103503f5c5bac4a17ac5cde
bananaplant7 <brooklyncheng2002@gmail.com>
2023-08-19 23:00:47 -0700

    fix videos link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar({ searchTerm }) {
             <div className="navbarLinkContainer"><a className='navbarLink' href={`https://www.google.com/search?q=${searchTerm}&tbm=isch`}>Images</a></div>
             <div className="navbarLinkContainer"><a className='navbarLink' href={`https://www.google.com/search?q=${searchTerm}&tbm=shop`}>Shopping</a></div>
             <div className="navbarLinkContainer"><a className='navbarLink' href={`https://www.google.com/search?q=${searchTerm}&tbm=bks`}>Books</a></div>
-            <div className="navbarLinkContainer"><a className='navbarLink' href={`https://www.google.com/search?q=${searchTerm}&vid=`}>Videos</a></div>
+            <div className="navbarLinkContainer"><a className='navbarLink' href={`https://www.google.com/search?q=${searchTerm}&tbm=vid`}>Videos</a></div>
             <div className="navbarLinkContainer"><a className='navbarLink' href={`https://www.google.com/maps/search/${searchTerm}/`}>Maps</a></div>
             <div className="navbarLinkContainer"><a className='navbarLink' href={`https://www.google.com/travel/flights?output=search&q=${searchTerm}`}>Flights</a></div>
             <div className="navbarLinkContainer"><a className='navbarLink' href={`https://www.google.com/finance/?output=search=${searchTerm}`}>Finance</a></div>
